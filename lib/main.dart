91f843026a48b0b1e6636fa9fb75462a6f559526
Jose Adrian <joseadria_v12@hotmail.com>
2025-04-19 18:15:44 -0600

    PageViewlisto

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food/pages/bottomnav.dart';
 import 'package:food/pages/home.dart';
 import 'package:food/pages/login.dart';
+import 'package:food/pages/onboard.dart';
 import 'package:food/pages/signup.dart';
 
 void main() {
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Signup(),
+      home: Onboard(),
     );
   }
 }
---
+++ lib/widget/content_model.dart
@@ -0,0 +1,21 @@
+class UnboardingContent {
+  String image;
+  String title;
+  String description;
+    UnboardingContent({required this.description, required this.image, required this.title});
+}
+
+List<UnboardingContent> contents=[
+  UnboardingContent(
+  description: 'Pick your food from our menu\n            More than 35 times', 
+  image: "images/screen1.png", 
+  title: 'Selection from our\n         Best Menu'),
+   UnboardingContent(
+  description: 'You can pay cash on delivery and\n       Card payment is available', 
+  image: "images/screen2.png", 
+  title: 'Easy and Online payment'),
+    UnboardingContent(
+  description: 'Deliver your food at you\n                Doorstep', 
+  image: "images/screen3.png", 
+  title: 'Quick Delivery at Your Doorstep'),
+];
