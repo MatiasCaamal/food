85c9d9ceb4783c7921f1370506b1c4d9cac6c3c7
Jose Adrian <joseadria_v12@hotmail.com>
2025-04-22 23:58:26 -0600

    forgotpasswordlisto.2:41:16

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:food/pages/bottomnav.dart';
+import 'package:food/pages/forgotpassword.dart';
 import 'package:food/pages/signup.dart';
 import 'package:food/widget/widget_support.dart';
 
@@ -141,10 +142,15 @@ class _LogInState extends State<LogIn> {
                             SizedBox(
                               height: 20.0,
                             ),
-                            Container(
-                                alignment: Alignment.topRight,
-                                child: Text("Forgot password",
-                                    style: Appwidget.semiBoldTextFeildStyle())),
+                            GestureDetector(
+                              onTap: (){
+                                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> Forgotpassword()));
+                              },
+                              child: Container(
+                                  alignment: Alignment.topRight,
+                                  child: Text("Forgot password",
+                                      style: Appwidget.semiBoldTextFeildStyle())),
+                            ),
                             SizedBox(height: 80.0),
                             GestureDetector(
                               onTap: () {
