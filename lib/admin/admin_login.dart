7d652fe49b99290a87d5807579d85a44af25c926
MatiasCaamal <matiascaamal@hotmail.com>
2025-05-25 00:01:36 -0600

    Terminando Admin Page

--- lib/admin/admin_login.dart
+++ lib/admin/admin_login.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:food/admin/home_admin.dart';
 
 class AdminLogin extends StatefulWidget {
   const AdminLogin({super.key});
@@ -9,6 +11,9 @@ class AdminLogin extends StatefulWidget {
 
 class _AdminLoginState extends State<AdminLogin> {
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  TextEditingController usernamecontroller = new TextEditingController();
+  TextEditingController userpasswordcontroller = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -52,10 +57,98 @@ class _AdminLoginState extends State<AdminLogin> {
                         elevation: 3.0,
                         borderRadius: BorderRadius.circular(20),
                         child: Container(
-                          height: MediaQuery.of(context).size.height/2.2,
-                          /* decoration: BoxDecoration(
-                            color: 
-                          ), */
+                          height: MediaQuery.of(context).size.height / 2.2,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 50.0,
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(
+                                    left: 20.0, top: 5.0, bottom: 5.0),
+                                margin: EdgeInsets.symmetric(horizontal: 20.0),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color:
+                                            Color.fromARGB(255, 160, 160, 147)),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Center(
+                                  child: TextFormField(
+                                    controller: usernamecontroller,
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please Enter Username';
+                                      }
+                                    },
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Username",
+                                        hintStyle: TextStyle(
+                                            color: Color.fromARGB(
+                                                255, 160, 160, 147))),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 40.0,
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(
+                                    left: 20.0, top: 5.0, bottom: 5.0),
+                                margin: EdgeInsets.symmetric(horizontal: 20.0),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color:
+                                            Color.fromARGB(255, 160, 160, 147)),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Center(
+                                  child: TextFormField(
+                                    controller: userpasswordcontroller,
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Please Enter Password';
+                                      }
+                                    },
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Password",
+                                        hintStyle: TextStyle(
+                                            color: Color.fromARGB(
+                                                255, 160, 160, 147))),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 40.0,
+                              ),
+                              GestureDetector(
+                                onTap: (){
+                                  LoginAdmin();
+                                },
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(vertical: 12.0),
+                                  margin: EdgeInsets.symmetric(horizontal: 20.0),
+                                  width: MediaQuery.of(context).size.width,
+                                  decoration: BoxDecoration(
+                                      color: Colors.black,
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: Center(
+                                    child: Text(
+                                      "LogIn",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       )
                     ],
@@ -65,5 +158,29 @@ class _AdminLoginState extends State<AdminLogin> {
         ),
       ),
     );
+  }
+
+  LoginAdmin() {
+    FirebaseFirestore.instance.collection("Admin").get().then((snapshot) {
+      snapshot.docs.forEach((result) {
+        if (result.data()['id'] != usernamecontroller.text.trim()) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+            "Your id is not correct",
+            style: TextStyle(fontSize: 18.0),
+          )));
+        } else if (result.data()['password'] !=
+            userpasswordcontroller.text.trim()) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+            "Your password is not correct",
+            style: TextStyle(fontSize: 18.0),
+          )));
+        } else {
+          Route route = MaterialPageRoute(builder: (context) => HomeAdmin());
+          Navigator.pushReplacement(context, route);
+        }
+      });
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:food/admin/admin_login.dart';
+import 'package:food/admin/home_admin.dart';
 import 'package:food/pages/bottomnav.dart';
 import 'package:food/pages/home.dart';
 import 'package:food/pages/login.dart';
@@ -46,7 +48,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Onboard(),
+      home: HomeAdmin(),
     );
   }
 }
