734f45e397376810aa7495624fa4d3b474b15d14
MatiasCaamal <matiascaamal@hotmail.com>
2025-05-27 01:54:50 -0600

    Terminado

--- lib/service/database.dart
+++ lib/service/database.dart
@@ -31,4 +31,15 @@ class DatabaseMethods {
         .collection("Cart")
         .add(userInfoMap);
   }
+
+  Future<Stream<QuerySnapshot>> getFoodCart(String id) async {
+    return await FirebaseFirestore.instance.collection("users").doc(id).collection("Cart").snapshots();
+  }
+
+  UpdateUserwallet(String id, String amount) async {
+    return await FirebaseFirestore.instance
+        .collection("users")
+        .doc(id)
+        .update({"Wallet": amount});
+  }
 }
